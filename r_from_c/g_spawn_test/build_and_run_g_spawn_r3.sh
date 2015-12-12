c29a110893259b2a13a75c5d647f7df4a532ecfe
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2015-12-13 01:53:40 +0300

    try spawn R with writing to in channel

--- r_from_c/g_spawn_test/build_and_run_g_spawn_r3.sh
+++ r_from_c/g_spawn_test/build_and_run_g_spawn_r3.sh
@@ -1,3 +1,1 @@
 gcc -g -Wall -o g_spawn_r3 g_spawn_r3.c $(pkg-config --cflags --libs glib-2.0)
-gcc -o helper helper.c
-./g_spawn_r3
--- r_from_c/g_spawn_test/g_spawn_r3.c
+++ r_from_c/g_spawn_test/g_spawn_r3.c
@@ -48,17 +48,23 @@ static gboolean cb_err_watch( GIOChannel   *channel,
 
 void init(void){
     GPid        pid;
-    gchar      *argv[] = { "./helper", NULL };
-    gint        out,
+    gchar      *argv[] = { "R", "--no-save", NULL };
+    //gchar      *argv[] = { "R", "--no-save", "--silent", NULL };
+    GSpawnFlags flags = G_SPAWN_DO_NOT_REAP_CHILD |
+                        G_SPAWN_SEARCH_PATH;
+    gint        in,
+                out,
                 err;
-    GIOChannel *out_ch,
+    GIOChannel *in_ch,
+               *out_ch,
                *err_ch;
     gboolean    ret;
 
     /* Spawn child process */
     ret = g_spawn_async_with_pipes( NULL, argv, NULL,
-            G_SPAWN_DO_NOT_REAP_CHILD, NULL,
-            NULL, &pid, NULL, &out, &err, NULL );
+            flags, NULL,
+            NULL, &pid,
+            &in, &out, &err, NULL );
     if( ! ret )
     {
         g_error( "SPAWN FAILED" );
@@ -67,6 +73,7 @@ void init(void){
 
     g_child_watch_add( pid, (GChildWatchFunc)cb_child_watch, NULL);
 
+    in_ch = g_io_channel_unix_new( in );
     out_ch = g_io_channel_unix_new( out );
     err_ch = g_io_channel_unix_new( err );
 
@@ -74,6 +81,21 @@ void init(void){
     g_io_add_watch( out_ch, G_IO_IN | G_IO_HUP, (GIOFunc)cb_out_watch, NULL);
     g_io_add_watch( err_ch, G_IO_IN | G_IO_HUP, (GIOFunc)cb_err_watch, NULL);
 
+    GString * cmd = g_string_new("5\n");
+    GError * error = NULL;
+    gsize bytes_written;
+    printf("Launching date in bash: %s\n", cmd->str);
+    g_io_channel_write_chars( in_ch,
+            cmd->str,
+            cmd->len,
+            &bytes_written,
+            &error);
+    if (error != NULL)
+    {
+        printf("%s\n", error->message);
+    }else{
+        printf("bytes: %u\n", bytes_written);
+    }
 }
 
 int main()
