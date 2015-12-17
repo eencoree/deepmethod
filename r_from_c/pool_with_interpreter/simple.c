58be3401ffc6a44c8c17e0872e7696388576e8d8
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2015-12-17 22:16:53 +0300

    fix get output from interpreters

--- r_from_c/pool_with_interpreter/simple.c
+++ r_from_c/pool_with_interpreter/simple.c
@@ -6,6 +6,7 @@ struct interpreter{
     GIOChannel *in,
                *out,
                *err;
+  GAsyncQueue * queue;
 };
 
 void write_to_interpreter(GIOChannel* in, GString* msg){
@@ -17,39 +18,24 @@ void write_to_interpreter(GIOChannel* in, GString* msg){
     g_io_channel_flush(in, NULL);
 }
 
-GString * create_command(int n){
-    GString * cmd = g_string_new("q <- ");
+GString * create_command_r(int n){
+    GString * cmd = g_string_new("flush.console();q <- ");
     char number[30];
     sprintf(number, "%d", n);
     cmd = g_string_append(cmd, number);
-    cmd = g_string_append(cmd, "\r\na <- q + 1\r\ncat(a,'\\n')\r\nflush.console()\r\n");
+    cmd = g_string_append(cmd, ";a = q + 1;cat(a,'\\n');flush.console()\r\n");
     g_print("VALUE: %s\n", cmd->str);
     return cmd;
 }
 
-gchar * read_from_interpreter(GIOChannel* out){
-    GMainContext * loop = g_main_context_default();
-
-    static const int check_delay = 50000;
-    GIOCondition cond = g_io_channel_get_buffer_condition(out);
-    g_print("cond: %d\n", cond);
-    while(G_IO_IN != cond){
-        g_main_context_iteration(loop, FALSE);
-        g_usleep(check_delay);
-        cond = g_io_channel_get_buffer_condition(out);
-        g_print("G_IO_IN != cond\n");
-    }
-    g_print("G_IO_IN == cond\n");
-
-    gchar *string;
-    gsize size;
-    GError *error = NULL;
-    GIOStatus ret = g_io_channel_read_to_end( out, &string, &size, &error );
-    g_print("ret: %d\n", ret);
-    if(error != NULL)
-        g_print("error: %s\n", error->message);
-    g_print("size: %u\n", size);
-    return string;
+GString * create_command_b(int n){
+    GString * cmd = g_string_new("echo \"$((");
+    char number[30];
+    sprintf(number, "%d", n);
+    cmd = g_string_append(cmd, number);
+    cmd = g_string_append(cmd, "+1))\"");
+    g_print("VALUE: %s\n", cmd->str);
+    return cmd;
 }
 
 gpointer pool_func(gpointer data, gpointer user_data){
@@ -59,35 +45,60 @@ gpointer pool_func(gpointer data, gpointer user_data){
     int val = *((int*)data);
 
     g_print("interpreter = %d, data = %d\n", intprt, val);
-    GString * cmd = create_command(val);
+    GString * cmd = create_command_r(val);
     write_to_interpreter(intprt->in, cmd);
-
-    gchar * result_str = read_from_interpreter(intprt->out);
-    g_print("Out: %s", result_str);
-    fflush(stdout);
-    g_free(result_str);
-
-    g_async_queue_push(queue, (gpointer)intprt);
+//g_usleep(100000);
+//    g_async_queue_push(queue, (gpointer)intprt);
     return 0;
 }
 
-/*static gboolean out_watch( GIOChannel   *channel,
+static gboolean out_watch( GIOChannel   *channel,
         GIOCondition  cond, gpointer data)
 {
     gchar *string;
     gsize  size;
-
+	GIOStatus status;
+  	GError *gerror = NULL;
+	struct interpreter *intprt = (struct interpreter*)data;
     if( cond == G_IO_HUP )
     {
-        g_io_channel_unref( channel );
+      	g_print("Cond is G_IO_HUP %d\n", (int)cond);
+      	g_io_channel_unref( channel );
         return( FALSE );
+    } else if( cond == G_IO_IN ) {
+      	g_print("Cond is G_IO_IN %d\n", (int)cond);
+  	fflush(stdout);
+    } else if( cond == G_IO_OUT ) {
+      	g_print("Cond is not G_IO_OUT %d\n", (int)cond);
+  	fflush(stdout);
     }
-
-    g_io_channel_read_line( channel, &string, &size, NULL, NULL );
+/*
+GIOStatus
+g_io_channel_read_to_end (GIOChannel *channel,
+                          gchar **str_return,
+                          gsize *length,
+                          GError **error);
+ */
+/*  status = g_io_channel_read_to_end (channel,
+                          &string,
+                          &size,
+                          &gerror);*/
+    status = g_io_channel_read_line( channel, &string, &size, NULL, NULL );
+  if (status != G_IO_STATUS_NORMAL) {
+    g_print("Status is not NORMAL %d\n", status);
+  } else {
+    g_print("Status is NORMAL %d\n", status);
+  }
+	fflush(stdout);
+  if (gerror != NULL) {
+    g_print(gerror->message);
+    g_error_free(gerror);
+  }
     g_print("Out: %s", string);
-    g_io_channel_flush(channel, NULL);
+/*    g_io_channel_flush(channel, NULL);*/
+  fflush(stdout);
     g_free( string );
-
+  g_async_queue_push(intprt->queue, (gpointer)intprt);
     return TRUE;
 }
 
@@ -109,17 +120,18 @@ static gboolean err_watch( GIOChannel   *channel,
     g_free( string );
 
     return TRUE;
-}*/
+}
 
-struct interpreter* init_interpreter(void){
-    gchar      *argv[] = { "R", "--vanilla", NULL };
+struct interpreter* init_interpreter(GAsyncQueue * queue){
+    gchar      *argvb[] = { "bash", "+x", NULL };
+    gchar      *argvr[] = { "R", "--no-save", "--silent", "--vanilla", NULL };
     gint        in,
                 out,
                 err;
     gboolean    ret;
-
+	GError *gerror = NULL;
     /* Spawn child process */
-    ret = g_spawn_async_with_pipes( NULL, argv, NULL,
+    ret = g_spawn_async_with_pipes( NULL, argvr, NULL,
             G_SPAWN_SEARCH_PATH | G_SPAWN_DO_NOT_REAP_CHILD, NULL,
             NULL, NULL,
             &in, &out, &err, NULL );
@@ -133,10 +145,15 @@ struct interpreter* init_interpreter(void){
     intprt->in = g_io_channel_unix_new( in );
     intprt->out = g_io_channel_unix_new( out );
     intprt->err = g_io_channel_unix_new( err );
-
-    /* Add watches to channels */
-    //g_io_add_watch( intprt->out, G_IO_IN | G_IO_HUP, (GIOFunc)out_watch, NULL);
-    //g_io_add_watch( intprt->err, G_IO_IN | G_IO_HUP, (GIOFunc)err_watch, NULL);
+  intprt->queue = queue;
+/*GIOStatus
+g_io_channel_set_flags (intprt->out,
+                        G_IO_FLAG_NONBLOCK,
+                        &gerror);
+ */
+      /* Add watches to channels */
+    g_io_add_watch( intprt->out, G_IO_IN | G_IO_OUT | G_IO_HUP, (GIOFunc)out_watch, intprt);
+    g_io_add_watch( intprt->err, G_IO_IN | G_IO_OUT | G_IO_HUP, (GIOFunc)err_watch, intprt);
 
     return intprt;
 }
@@ -144,7 +161,7 @@ struct interpreter* init_interpreter(void){
 
 int main(){
     int i;
-    int num_processors = 1;//g_get_num_processors();
+    int num_processors = 4;//g_get_num_processors();
     g_print("Number processors: %d\n", num_processors);
 
     g_print("Create queue\n");
@@ -153,29 +170,29 @@ int main(){
     g_print("Push interpreters to queue\n");
     struct interpreter* intprt;
     for(i = 0; i < num_processors; i++){
-        intprt = init_interpreter();
+        intprt = init_interpreter(q);
         g_async_queue_push(q, (gpointer)intprt);
     }
-    g_print("Number interpreters in queue: %d\n", g_async_queue_length(q));
+    g_print("Number tasks in queue: %d\n", g_async_queue_length(q));
 
     g_print("Create pool\n");
     GThreadPool *pool = g_thread_pool_new(
             (GFunc)pool_func,
             (gpointer)q,
-            num_processors,
+            g_get_num_processors(),
             TRUE, NULL);
 
     g_print("Push tasks to pool\n");
-    int num_tasks = 1;//2 * num_processors;
-    g_print("Max number tasks in queue: %d\n", num_tasks);
+    int num_tasks = 20;//2 * num_processors;
     int* tasks = g_new(int, num_tasks);
     for(i = 0; i < num_tasks; i++){
         tasks[i] = i;
         g_thread_pool_push (pool, (gpointer)(tasks+i), NULL);
     }
-    g_print("Number tasks in queue: %d\n", g_thread_pool_get_num_unused_threads());
 
-    g_thread_pool_free(pool, FALSE, TRUE);
+    GMainLoop *loop = g_main_loop_new (NULL, FALSE);
+    g_main_loop_run (loop);
+
     g_print("End main\n");
     return 0;
 }
