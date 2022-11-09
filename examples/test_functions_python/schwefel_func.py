a75c7e3c072e6c64b842108cc242506dcd9fa718
Konstantin Kozlov <mackoel@gmail.com>
2022-11-09 08:55:47 +0300

    Added examples folder

---
+++ examples/test_functions_python/schwefel_func.py
@@ -0,0 +1,17 @@
+import sys
+from math import sin, sqrt
+
+
+def schwefel(xx: list):
+    sum = 0
+    for x in xx:
+        sum += x * sin(sqrt(abs(x)))
+
+    return 418.9829 * len(xx) - sum
+
+
+xx = list()
+for i in sys.argv[1:]:
+    xx.append(float(i))
+
+print(schwefel(xx))
